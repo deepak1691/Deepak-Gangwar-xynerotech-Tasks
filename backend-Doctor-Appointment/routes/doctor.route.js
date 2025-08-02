1692bbd8752d93e39f164ad08fc46a53912055be
deepak1691 <deepakgangwar321@gmail.com>
2025-08-03 00:14:47 +0530

    Update doctor.route.js

--- backend-Doctor-Appointment/routes/doctor.route.js
+++ backend-Doctor-Appointment/routes/doctor.route.js
@@ -7,15 +7,15 @@ import {createDoctors,deleteDoctor,getDoctors,getSpecificDoctor,updateDoctor} fr
 const router=express.Router();
 
 router.route("/")
-    .post(validate(doctorSchemaValidate), wrapAsync(createDoctors))// Create a Doctor (POST /doctors) (http://localhost:3000/api/v1/doctors)
-    .get(wrapAsync(getDoctors)); // Get All Doctors (GET /doctors) ----> http://localhost:3000/api/v1/doctors
+    .post(validate(doctorSchemaValidate), wrapAsync(createDoctors))// Create a Doctor ----> http://localhost:3000/api/v1/doctors
+    .get(wrapAsync(getDoctors)); // Get All Doctors ----> http://localhost:3000/api/v1/doctors
 
 
 // the routes which match with :/id
     router.route("/:id")
-    .get(wrapAsync(getSpecificDoctor)) // Get a Doctor by ID (GET /doctors/:id) ----> http://localhost:3000/api/v1/doctors/:id
-    .put(validate(doctorSchemaValidate),wrapAsync(updateDoctor))      // Update a Doctor (PUT /doctors/:id)  ----> http://localhost:3000/api/v1/doctors/:id
-    .delete(wrapAsync(deleteDoctor)); // Delete a Doctor (DELETE /doctors/:id)  ----> http://localhost:3000/api/v1/doctors/:id
+    .get(wrapAsync(getSpecificDoctor)) // Get a Doctor by ID  ----> http://localhost:3000/api/v1/doctors/:id
+    .put(validate(doctorSchemaValidate),wrapAsync(updateDoctor))  // Update a Doctor   ----> http://localhost:3000/api/v1/doctors/:id
+    .delete(wrapAsync(deleteDoctor)); // Delete a Doctor ----> http://localhost:3000/api/v1/doctors/:id
 
     
 
